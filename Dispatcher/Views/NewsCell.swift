7d68a7491453391a73e6123d0ada8b974ed0a87f
Noam Kurtzer <noamk@moveo.co.il>
2022-03-03 17:27:54 +0200

    Add custom table cells: Notification, News, Saved Article

---
+++ Dispatcher/Views/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  Dispatcher
+//
+//  Created by Noam Kurtzer on 03/03/2022.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
