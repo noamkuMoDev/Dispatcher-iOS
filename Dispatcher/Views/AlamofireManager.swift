d6b87ac5b3423ebe7577cccfda22d1060d42d92d
Noam Kurtzer <noamk@moveo.co.il>
2022-03-15 18:04:49 +0200

    Added Alamofire pod and AlamofireManager general reusable class

---
+++ Dispatcher/Views/AlamofireManager.swift
@@ -0,0 +1,50 @@
+import Foundation
+import Alamofire
+
+class AlamofireManager: NSObject {
+    
+    var url: String!
+    var headers = HTTPHeaders()
+    var parameters = Parameters()
+    var encoding: ParameterEncoding = JSONEncoding.default
+    
+    init( data: [String:Any]? = nil, from url: String) {
+        super.init()
+        
+        self.url = url
+        ["Accept": "application/json", "Content-type": "application/json"].forEach({self.headers.add(name: $0.key, value: $0.value)})
+        if let safeData = data {
+            safeData.forEach{ parameters.updateValue($0.value, forKey: $0.key) }
+        }
+    }
+    
+    func executeGetQuery<T>(completion: @escaping (Result<T, Error>) -> Void) where T: Codable {
+        AF.request(url, method: .get).responseData(completionHandler: { response in
+            do {
+                switch response.result {
+                case .success:
+                    completion(.success(try JSONDecoder().decode(T.self, from: response.data ?? Data())))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            } catch let error {
+                print(error)
+            }
+        })
+    }
+    
+    func executePostQuery<T>(completion: @escaping (Result<T, Error>) -> Void) where T: Codable {
+        AF.request(url, method: .post, parameters: parameters, encoding: encoding, headers: headers).responseData(completionHandler: { response in
+            do {
+                switch response.result {
+                case .success:
+                    completion(.success(try JSONDecoder().decode(T.self, from: response.data ?? Data())))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            } catch let error {
+                print(error)
+            }
+        })
+    }
+}
